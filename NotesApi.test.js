7f3edf6e0b88b7f9001766c963e5d5c3b332e7bf
mikitaro-27 <matt.diak@gmail.com>
2022-02-03 15:42:30 +0000

    create notesApi class and a loadNotes method

--- NotesApi.test.js
+++ NotesApi.test.js
@@ -1,12 +1,16 @@
-require('jest-fetch-mock').enableMocks();
-
 const NotesApi = require('./notesapi');
 
+require('jest-fetch-mock').enableMocks();
+
 describe('NotesApi class', () => {
-   it('calls fetch and loads repo info', async () => {
+   it('calls fetch and loads notes', async () => {
        const api = new NotesApi();
        fetch.mockResponseOnce(JSON.stringify(
            ['test note']))
+        
+        api.loadNotes((notes) => {
+            expect(notes[0]).toBe('test note');
+        })
    })
 })
 
